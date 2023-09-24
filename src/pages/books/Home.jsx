a39507de30db216be760d075f6074b173d14c929
Roman Arenas <roman.ea3232@gmail.com>
2023-09-24 18:52:41 -0300

    ...

--- src/pages/books/Home.jsx
+++ src/pages/books/Home.jsx
@@ -1,10 +1,11 @@
+import { useNavigate } from "react-router-dom"
 import { Header, Nabvar } from "../../components"
 import { useAuth } from "../../hooks"
 import './books.css'
 
 export const Home = () => {
 
-
+  const navigate = useNavigate();
   const { auth } = useAuth();
 
   return (
@@ -16,14 +17,14 @@ export const Home = () => {
           <h2 className="tip">Dowload</h2>
           <h6 className="second-text">Descarga y comparte informacion</h6>
         </div>
-        <div className="panel green">
+        <div className="panel green" onClick={()=> navigate('../login')}>
         <h2 className="tip">Inicia Sesion</h2>
           <h6 className="second-text">únete a la comunidad</h6>
         </div>
         {
           auth.emal && <div className="panel red">
-            <p className="tip">Hover Me</p>
-            <p className="second-text">Lorem Ipsum</p>
+            <p className="tip">Salir!</p>
+            <p className="second-text">Vuelve pronto</p>
           </div>
         }
 
